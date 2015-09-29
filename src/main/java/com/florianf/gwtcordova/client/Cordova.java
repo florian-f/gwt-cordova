9e39c79888906f8132a40a62f1302e7569859135
Florian Friemel <florianfriemel@gmail.com>
2015-09-29 21:42:00 +0200

    clean up

--- src/main/java/com/florianf/gwtcordova/client/Cordova.java
+++ src/main/java/com/florianf/gwtcordova/client/Cordova.java
@@ -2,6 +2,7 @@ package com.florianf.gwtcordova.client;
 
 import com.florianf.gwtcordova.client.elemental.Event;
 import com.florianf.gwtcordova.client.elemental.EventListener;
+import com.florianf.gwtcordova.client.elemental.Function;
 import com.florianf.gwtcordova.client.plugin.device.Device;
 import com.florianf.gwtcordova.client.plugin.dialogs.Notification;
 import com.florianf.gwtcordova.client.plugin.network.Connection;
@@ -15,9 +16,6 @@ import com.google.gwt.user.client.Window;
  */
 public abstract class Cordova {
 
-    // gwtphonegap
-    private static final int INIT_TICK = 10;
-    // gwtphonegap
     private static boolean deviceReady;
 
     public static native Device getDevice()/*-{
@@ -48,65 +46,18 @@ public abstract class Cordova {
     public static com.florianf.gwtcordova.client.elemental.Document getDocument() {
         return ( com.florianf.gwtcordova.client.elemental.Document) Document.get();
     }
-    // gwtphonegap
-//    public static void initializeCordova(){
-//        initializeCordova(10000);
-//    }
 
-    // gwtphonegap
-    public static void initializeCordova(final int timeoutInMs,  DeviceReadyEvent.Listener listener) {
+    public static void init(DeviceReadyEvent.Listener listener) {
 
         com.florianf.gwtcordova.client.elemental.Document document = (com.florianf.gwtcordova.client.elemental.Document) Document.get();
-//        document.addEventListener(DeviceReadyEvent.NAME, new DeviceReadyEvent.Listener() {
-//
-//            @Override
-//            protected void handleEvent(DeviceReadyEvent deviceReadyEvent) {
-//                deviceReady = true;
-//                listener.handleEvent(deviceReadyEvent);
-//            }
-//        });
-//        document.addEventListener("deviceready", new EventListener() {
-//            public void handleEvent(Event event) {
-//                Window.alert("Hi");
-//            }
-//
-//        });
-        final long end = System.currentTimeMillis() + timeoutInMs;
-        if (isPhoneGapInitialized()) {
-
-//            firePhoneGapAvailable();
-
-        } else {
-            Timer timer = new Timer() {
-
-                @Override
-                public void run() {
-                    if (isPhoneGapInitialized()) {
-//                        firePhoneGapAvailable();
-                        return;
-                    }
-
-//                    if (System.currentTimeMillis() - end > 0) {
-//                        handlerManager.fireEvent(new PhoneGapTimeoutEvent());
-//                    } else {
-                        schedule(INIT_TICK);
-//                    }
-
-                }
-            };
-
-            timer.schedule(INIT_TICK);
-        }
+        document.addEventListener("deviceready", event -> {
+            deviceReady = true;
+            listener.handleEvent((DeviceReadyEvent) event);
+            return null;
+        }, true);
     }
 
-    // gwtphonegap
-    public static boolean isPhoneGapInitialized() {
+    public static boolean isDeviceReady() {
         return deviceReady;
     }
-
-    // gwtphonegap
-    protected void nativeDeviceReady() {
-        deviceReady = true;
-    }
-
 }
--- src/main/java/com/florianf/gwtcordova/client/elemental/EventListener.java
+++ src/main/java/com/florianf/gwtcordova/client/elemental/EventListener.java
@@ -4,7 +4,6 @@ import com.google.gwt.core.client.js.JsExport;
 import com.google.gwt.core.client.js.JsType;
 
 @JsType
-@JsExport
 public interface EventListener {
     void handleEvent(Event event);
 }
