244bd68bc4336aa3aeb92212eb5e33a1e5b4c5d0
Florian Friemel <florianfriemel@gmail.com>
2016-06-27 21:17:00 +0200

    fix screen-orientation

--- src/main/java/com/florianf/gwtcordova/client/plugin/screenorientation/ios/Screen.java
+++ src/main/java/com/florianf/gwtcordova/client/plugin/screenorientation/ios/Screen.java
@@ -1,8 +1,8 @@
-package com.florianf.gwtcordova.client.plugin.screenorientation;
+package com.florianf.gwtcordova.client.plugin.screenorientation.ios;
 
 import com.google.gwt.core.client.js.JsNamespace;
-import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 /**
@@ -18,6 +18,6 @@ public class Screen {
     public static native void unlockOrientation();
 
     @JsProperty(name = "orientation")
-    public static native ScreenOrientation getOrientation();
+    public static native String getOrientation();
 
 }
